f3a87cd11f5e74cf9a39a3c34176116991c1757e
Derrick Gilland <dgilland@gmail.com>
2014-03-30 19:34:09 -0400

    Import metadata in package __init__.py.

--- carafe/__init__.py
+++ carafe/__init__.py
@@ -16,5 +16,12 @@ from .core import (
 	auth
 )
 
-__version__ = '0.1.5'
-__author__ = 'Derrick Gilland <dgilland@gmail.com>'
+from .__meta__ import (
+    __title__,
+    __summary__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__
+)
