8352be51341ba9d648dcdbfe3f8f4abfcd71b714
Derrick Gilland <dgilland@gmail.com>
2014-04-03 16:26:08 -0400

    Release v0.2.2.

--- carafe/__meta__.py
+++ carafe/__meta__.py
@@ -15,7 +15,7 @@ __title__ = 'carafe'
 __summary__ = 'Flask application factory with extensions geared towards JSON APIs'
 __url__ = 'https://github.com/dgilland/carafe'
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __author__ = 'Derrick Gilland'
 __email__ = 'dgilland@gmail.com'
